780432f9260168a23d1680f40c3d3cc6398e8e0a
xerysherry <xerysherry@aliyun.com>
2018-03-11 21:28:31 +0800

    .

---
+++ Assets/Add-ins/Route/Scripts/Extends/PlainAxes.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 十字对象
+/// </summary>
+[ExecuteInEditMode]
+public class PlainAxes : MonoBehaviour
+{
+    static readonly float kOSize = 0.025f;
+    static readonly float kAxesSize = 0.3f;
+#if UNITY_EDITOR
+    void Update()
+    {
+        if(colorset == ColorSet.kNone)
+            return;
+        color = color_array[(int)colorset];
+        colorset = ColorSet.kNone;
+    }
+
+    void OnDrawGizmos()
+    {
+        var rot = transform.rotation;
+        var p = transform.position;
+        var s = transform.lossyScale.x * kAxesSize;
+        var v = rot * Vector3.up * s;
+        Gizmos.color = color;
+        Gizmos.DrawRay(p, v);
+        Gizmos.DrawRay(p, -v);
+        v = rot * Vector3.forward * s;
+        Gizmos.DrawRay(p, v);
+        Gizmos.DrawRay(p, -v);
+        v = rot * Vector3.right * s;
+        Gizmos.DrawRay(p, v);
+        Gizmos.DrawRay(p, -v);
+
+        Gizmos.DrawSphere(p, kOSize);
+    }
+
+    static readonly Color[] color_array = new Color[]
+    {
+        Color.white,
+        Color.green,
+        Color.red,
+        Color.blue,
+        Color.black,
+        Color.cyan,
+        Color.gray,
+        Color.white,
+        Color.magenta,
+        Color.yellow,
+    };
+    /// <summary>
+    /// 颜色
+    /// </summary>
+    public Color color = Color.green;
+    /// <summary>
+    /// 快速设置
+    /// </summary>
+    public enum ColorSet
+    {
+        kNone = 0,
+        kGreen,
+        kRed,
+        kBlue,
+        kBlack,
+        kCyan,
+        kGray,
+        kWhite,
+        kMegenta,
+        kYellow,
+    }
+    public ColorSet colorset;
+#endif
+}
--- Assets/Add-ins/Route/Scripts/LookAtSomething.cs
+++ Assets/Add-ins/Route/Scripts/LookAtSomething.cs
@@ -6,26 +6,76 @@ public class LookAtSomething : MonoBehaviour
 {
 	void Update () 
     {
-        if(target == null)
-            return;
-
-        //var dir = (target.transform.position - transform.position).normalized;
-        transform.LookAt(target.transform);
+        if(target == Target.TargetGameObject)
+        {
+            if(target_obj == null)
+                return;
+            transform.LookAt(target_obj.transform);
+        }
+        else
+        {
+            transform.LookAt(target_pos);
+        }
 	}
 
 #if UNITY_EDITOR
+    static readonly float kStep = 1.0f;
+
     void OnDrawGizmos()
     {
-        if(target == null)
-            return;
+        Vector3 pos = Vector3.zero;
+        Gizmos.color = Color.yellow;
+        if(target == Target.TargetGameObject)
+        {
+            if(target_obj == null)
+                return;
+            pos = target_obj.transform.position;
+        }
+        else
+        {
+            pos = target_pos;
+        }
 
-        Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, 0.5f);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(target.transform.position, 0.5f);
-        Gizmos.DrawLine(transform.position, target.transform.position);
+        Gizmos.DrawWireSphere(pos, 0.5f);
+        Gizmos.DrawLine(transform.position, pos);
+
+        Vector3 v = (pos - transform.position);
+        Vector3 dir = v.normalized;
+        float length = v.magnitude;
+        float l = 0;
+        v = v.normalized;
+        
+        if(v.x == 0 && Mathf.Abs(v.y) == 1 && v.z == 0)
+            v.z += 0.1f;
+        Vector3 axis = Vector3.Cross(v, Vector3.up);
+        axis = Vector3.Cross(v, axis);
+        var p1 = (Quaternion.AngleAxis(160, axis) * dir).normalized * 0.3f;
+        var p2 = (Quaternion.AngleAxis(-160, axis) * dir).normalized * 0.3f;
+
+        axis = Vector3.Cross(axis, dir);
+        var p3 = (Quaternion.AngleAxis(160, axis) * dir).normalized * 0.3f;
+        var p4 = (Quaternion.AngleAxis(-160, axis) * dir).normalized * 0.3f;
+
+        
+        while(l < length)
+        {
+            var p = transform.position + dir * l;
+            Gizmos.DrawRay(p, p1);
+            Gizmos.DrawRay(p, p2);
+            Gizmos.DrawRay(p, p3);
+            Gizmos.DrawRay(p, p4);
+            l += kStep;
+        }
     }
 #endif
 
-    public GameObject target;
+    public enum Target
+    {
+        TargetGameObject,
+        TargetPosition,
+    }
+    public Target target = Target.TargetGameObject;
+    public GameObject target_obj;
+    public Vector3 target_pos;
 }
