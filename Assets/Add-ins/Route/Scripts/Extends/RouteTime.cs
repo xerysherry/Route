de8dc39cfa118c141c0e5bf6eb99576600d0a08f
xerysherry <xerysherry@aliyun.com>
2018-03-18 17:23:35 +0800

    .

--- Assets/Add-ins/Route/Scripts/Extends/RouteTime.cs
+++ Assets/Add-ins/Route/Scripts/Extends/RouteTime.cs
@@ -1,10 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿/* Copyright 2018 xerysherry
+ * 
+ * Permission is hereby granted, free of charge, to any person 
+ * obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of 
+ * the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall 
+ * be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ */
+ 
+ using UnityEngine;
 
+[AddComponentMenu("Route/Route Time")]
 public class RouteTime : RouteComponent
 {
-	// Use this for initialization
 	void Start ()
     {
         RefreshConfig();
@@ -12,14 +33,17 @@ public class RouteTime : RouteComponent
         controller_.Update(start_time);
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
         if(obj == null)
             return;
         controller_.SetRotateEnable(rotation);
         controller_.Update(delta, obj);
-	}
+        if(eular != Vector3.zero)
+            obj.transform.rotation = obj.transform.rotation * Quaternion.Euler(eular);
+        if(position != Vector3.zero)
+            obj.transform.position = obj.transform.position + position;
+    }
 
     float delta
     {
@@ -47,17 +71,52 @@ public class RouteTime : RouteComponent
         }
     }
 
+    /// <summary>
+    /// 轨迹对象
+    /// </summary>
     public GameObject obj;
+    /// <summary>
+    /// 面向曲线切线方向
+    /// </summary>
     public bool rotation = true;
+    /// <summary>
+    /// 是否反向
+    /// </summary>
     public bool negative = false;
+    /// <summary>
+    /// 开始时间偏移
+    /// </summary>
     public float start_time = 0;
     public enum Mode
     {
+        /// <summary>
+        /// Time.deltaTime
+        /// </summary>
         DELTA_TIME,
+        /// <summary>
+        /// Time.fixedDeltaTime
+        /// </summary>
         FIXED_DELTA_TIME,
+        /// <summary>
+        /// Time.fixedUnscaledDeltaTime
+        /// </summary>
         FIXED_UNSCALED_DELTA_TIME,
+        /// <summary>
+        /// 自定义差量
+        /// </summary>
         CUSTOM_TIME,
     }
     public Mode mode = Mode.DELTA_TIME;
+    /// <summary>
+    /// 自定义差量
+    /// </summary>
     public float custom_delta = 0;
+    /// <summary>
+    /// 附加偏移
+    /// </summary>
+    public Vector3 position = Vector3.zero;
+    /// <summary>
+    /// 附加旋转
+    /// </summary>
+    public Vector3 eular = Vector3.zero;
 }
--- Assets/Add-ins/Route/Scripts/RouteConfig.cs
+++ Assets/Add-ins/Route/Scripts/RouteConfig.cs
@@ -1,7 +1,27 @@
-﻿using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+﻿/* Copyright 2018 xerysherry
+ * 
+ * Permission is hereby granted, free of charge, to any person 
+ * obtaining a copy of this software and associated documentation 
+ * files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of 
+ * the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall 
+ * be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
+ */
+
+using UnityEngine;
 using Bezier3D = RouteMath.Bezier3D;
 
 [ExecuteInEditMode]
@@ -134,7 +154,7 @@ public class RouteConfig : MonoBehaviour
             step += l;
             if(step >= step_length)
             {
-                ArrayUtility.Add(ref pt0.steps_, t);
+                UnityEditor.ArrayUtility.Add(ref pt0.steps_, t);
                 step -= step_length;
             }
             length += l;
